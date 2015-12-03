677be4de42e6254cfc3a311e8930dcfd206f223a
langfei yang <185601452@qq.com>
2015-12-03 08:58:54 +0800

    op

---
+++ app/src/main/java/com/ylf/jucaipen/newtest/com/ylf/jucaipen/view/com/ylf/jucaipen/activity/ImageTest.java
@@ -0,0 +1,7 @@
+package com.ylf.jucaipen.newtest.com.ylf.jucaipen.view.com.ylf.jucaipen.activity;
+
+/**
+ * Created by Administrator on 2015/12/1.
+ */
+public class ImageTest {
+}
---
+++ app/src/main/java/com/ylf/jucaipen/newtest/com/ylf/jucaipen/view/com/ylf/jucaipen/view/MyImageView.java
@@ -0,0 +1,7 @@
+package com.ylf.jucaipen.newtest.com.ylf.jucaipen.view.com.ylf.jucaipen.view;
+
+/**
+ * Created by Administrator on 2015/12/1.
+ */
+public class MyImageView {
+}
